780d46c0d86c42145b7a141cacfec93853442601
Mohamed Gouda <goda61@gmail.com>
2024-04-20 16:31:00 +0200

    Add sign-up view

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import User
+from .forms import UserRegisterForm
+
+
 # Create your views here.
+
+def RegisterView (request):
+    
+    form = UserRegisterForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'userauths/sign-up.html', context)
