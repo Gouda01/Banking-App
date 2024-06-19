fe2d473ea12a23c36a163747dc49c9274d3eeecf
Mohamed Gouda <goda61@gmail.com>
2024-06-19 14:57:18 +0300

    Add Signals Automatically create bank account for users when they create an account - L15

--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from shortuuid.django_fields import ShortUUIDField
+from django.db.models.signals import post_save
 
 from userauths.models import User
 
@@ -59,4 +60,16 @@ class Account(models.Model):
             return str(self.user)
         except:
             return "Account Model"
+        
+
+def create_account (sender, instance, created, **kwargs):
+    if created:
+        Account.objects.create(user=instance)
+
+def save_account(sender, instance, **kwargs):
+    instance.account.save()
+
+post_save.connect(create_account, sender=User)
+post_save.connect(save_account, sender=User)
+
     
