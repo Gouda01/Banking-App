acee2ce87a35337a972b85115563c815c6578a6c
Mohamed Gouda <goda61@gmail.com>
2024-06-21 17:15:48 +0300

    Add Search Users using Account Number or Accouny ID - finish L21

--- core/transfer.py
+++ core/transfer.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from account.models import Account
 
@@ -8,9 +9,17 @@ from account.models import Account
 def search_users_account_number(request):
 
     accounts = Account.objects.all()
+    query = request.POST.get("account_number")
+
+    if query :
+        accounts = Account.objects.filter(
+            Q(account_number=query)|
+            Q(account_id=query)
+        )
 
     context = {
         'accounts' : accounts,
+        'query' : query,
     }
 
     return render (request, 'transfer/search-user-by-account-number.html', context)
