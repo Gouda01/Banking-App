44c90999f13eaba9b2c1d6e099a3c15ee2527808
Mohamed Gouda <goda61@gmail.com>
2024-06-30 16:30:44 +0200

    Add Listing Payment Transaction History - L28 - M6:50

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "account"
 
 urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.account, name='account'),
     path('kyc-reg/', views.kyc_registration, name='kyc-reg'),
 ]
